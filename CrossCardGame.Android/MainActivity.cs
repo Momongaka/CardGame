fc20dccb665808d2cee0b58258eec60b37301085
Daniel <daniel.formas@gmail.com>
2019-10-15 13:05:03 -0500

    First

---
+++ CrossCardGame.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CrossCardGame.Android
+{
+    [Activity(Label = "CrossCardGame", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            XF.Material.Droid.Material.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
