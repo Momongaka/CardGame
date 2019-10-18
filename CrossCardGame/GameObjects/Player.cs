d32af08f811e3b3ad8c055242a43f9922dd3e4b2
Daniel <daniel.formas@gmail.com>
2019-10-18 12:25:27 -0500

    Fixed Player Setup Sheet

--- CrossCardGame/GameObjects/Player.cs
+++ CrossCardGame/GameObjects/Player.cs
@@ -2,14 +2,14 @@ namespace CrossCardGame.GameObjects
 {
     public class Player
     {
-        public int ID { get; private set; }
+        public int IdNum { get; private set; }
         public string Name { get; private set; }
         public string Gender { get; private set; }
         public string GenderPreference { get; private set; }
 
         public Player(int id, string name, string gender, string genderPreference)
         {
-            ID = id;
+            IdNum = id;
             Name = name;
             Gender = gender;
             GenderPreference = genderPreference;
